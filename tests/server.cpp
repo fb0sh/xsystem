48d51ef37f746a35ad134d3c16cd6412180d83fa
freetbash <88578981+freetbash@users.noreply.github.com>
2023-01-16 16:41:53 +0800

    Create server.cpp

---
+++ tests/server.cpp
@@ -0,0 +1,22 @@
+#include "../xsystem.hpp"
+using xsystem::net::Socket;
+#include <iostream>
+using std::cin,std::cout;
+
+int main(){
+  Socket::Init();
+  
+  Socket s(AF_INET, SOCK_STREAM, 0);
+  s.Bind("192.168.0.14", 3147);
+  s.Listen(5);
+  
+  auto c = s.Accept();
+  
+  char buf[1024]="Hello!";
+  c.Send(buf,1024,0);
+  c.Recv(buf,1024,0);
+  cout << buf << endl;
+  
+  Socket::Exit();
+  return 0;
+}
