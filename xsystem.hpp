9ff650548b16e159c55b0193dcc4f521e7592009
bash <bash@freet.tech>
2023-01-25 21:57:59 +0800

    添加tools logger

--- xsystem.hpp
+++ xsystem.hpp
@@ -74,9 +74,16 @@ using std::vector;
 #include <memory>
 using std::shared_ptr;
 
+#include <mutex>
+using std::mutex;
+
+#include <chrono>
 #include <time.h>
 #include <stdint.h>
 #include <sys/stat.h>
+
+
+
 
 namespace xsystem {
 
@@ -592,7 +599,7 @@ namespace xsystem {
 				SSL_set_fd(client->ssl, client->fd);
 				if(SSL_accept(client->ssl) == -1) {
 					perror("SSL accpet error");
-					exit(-1);
+					exit(EXIT_FAILURE);
 				}
 			#endif
 				return client;
@@ -1120,6 +1127,71 @@ HTTP_CONTENT:
 		}
 
 	};// namespace file
+
+	namespace tools {
+		class Logger {
+		private:
+			mutex f;
+			FILE *log_file = NULL;
+			int file_ok = 0;
+			int debug_ok = 0;
+
+			void BASE(string status, string text) {
+				time_t now = time(&now);
+				struct tm ti;
+				localtime_s(&ti, &now);
+				std::chrono::time_point<std::chrono::system_clock, std::chrono::milliseconds> tp = std::chrono::time_point_cast<std::chrono::milliseconds>(std::chrono::system_clock::now());
+				auto tmp = std::chrono::duration_cast<std::chrono::milliseconds>(tp.time_since_epoch());
+				std::time_t timestamp = tmp.count();
+				string time_date = "[" + to_string(ti.tm_year + 1900)
+					+ "-" + to_string(ti.tm_mon + 1)
+					+ "-" + to_string(ti.tm_mday)
+					+ " " + to_string(ti.tm_hour)
+					+ ":" + to_string(ti.tm_min)
+					+ ":" + to_string(ti.tm_sec)
+					+ "." + to_string(timestamp)
+					+ "] ";
+				string all = time_date + "[" + status + "] " + text + "\n";
+
+				cout << all;
+				if(file_ok) {
+					if(this->f.try_lock()) {
+						fprintf(this->log_file, "%s", all.c_str());
+						this->f.unlock();
+					}
+				}
+			}
+		public:
+			Logger(const char *file_path, int debug = 0, const char *mode = "a") {
+				this->debug_ok = debug;
+				this->log_file = fopen(file_path, mode);
+				if(this->log_file != NULL) this->file_ok = 1;
+				else perror("File can not create"), exit(EXIT_FAILURE);
+			}
+			Logger() {}
+			~Logger() {}
+
+			void info(string text) { this->BASE("INFO", text); }
+			void debug(string text) {
+				if(this->debug_ok) this->BASE("DEBUG", text);
+			}
+			void warn(string text) { this->BASE("WARN", text); }
+			void fatal(string text) { this->BASE("FATAL", text); }
+			void error(string text) { this->BASE("ERROR", text); }
+			void common(string text) {
+				text += "\n";
+				cout << text;
+				if(file_ok) {
+					if(this->f.try_lock()) {
+						fprintf(this->log_file, "%s", text.c_str());
+						this->f.unlock();
+					}
+				}
+			}
+		};
+
+
+	};
 
 };// namespace xsystem
 
